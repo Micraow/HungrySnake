0f229c49ac64470d4abd7fda2e063b9fe41263e2
micraow <15190450708@163.com>
2023-08-02 13:05:44 +0800

    prepare for ncurses

--- src/ncurses.cpp
+++ src/ncurses.cpp
@@ -1,11 +1,13 @@
 #include <ncurses.h>
+
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    initscr();
-    cbreak();
-    noecho();
+int main()
+{   
+    /** 每次输出后记得refresh,并设置等待**/
+    initscr();            /* Start curses mode        */
+    raw();                /* Line buffering disabled  */
+    noecho();             /* Don't echo() while we do getch */
 
-    return 0;
+
 }
